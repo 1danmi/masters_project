b65b11f8ffdbb63a7d3a3ef12c4ee3e901f7be29
1danmi <danielmiller20@gmail.com>
2025-09-07 07:45:08 +0300

    Add persistence utilities for compact Bert2Vec model

---
+++ src/__init__.py
@@ -0,0 +1,4 @@
+from .bert_2_vec_model import Bert2VecModel
+from .compact_bert2vec_model import CompactBert2VecModel, CompactTokenEntry
+
+__all__ = ["Bert2VecModel", "CompactBert2VecModel", "CompactTokenEntry"]
